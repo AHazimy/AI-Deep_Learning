ff8143eae923a9ac1f7479595c78399c4f48b001
AHazimy <aly.hazimy.95@gmail.com>
2022-12-31 22:19:43 +0200

    Model Finshed

--- FaceDetection/api.py
+++ FaceDetection/api.py
@@ -16,22 +16,8 @@ def face_extractor(img):
         
     return cropped_face
 
-# cap = cv2.VideoCapture(0)
-count = 0
 
-if face_extractor(frame) is not None:
-    count+=1
-    face = cv2.resize(face_extractor(frame), (200,200))
-    face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
-    
-    file_name_path = ''
-    cv2.imwrite(file_name_path, face)
-    cv2.putText(face, str(count), (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
-    cv2.imshow('Face Cropper', face)
-    
-else:
-    print("Face not found")
-    pass
+
 
 
 
--- FaceDetection/frames_extactor.py
+++ FaceDetection/frames_extactor.py
@@ -2,21 +2,25 @@
 import cv2
 import os
 
+from api import face_extractor
+
+face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
 # Read the video from specified path
 cam = cv2.VideoCapture("./db_train_real/client001_session01_webcam_authenticate_adverse_1.mov")
 
 try:
 	
 	# creating a folder named data
-	if not os.path.exists('data'):
-		os.makedirs('data')
+	if not os.path.exists('data/train'):
+		os.makedirs('data/train')
 
 # if not created then raise error
 except OSError:
 	print ('Error: Creating directory of data')
 
-# frame
-currentframe = 0
+
+count = 0
 
 while(True):
 	
@@ -24,20 +28,23 @@ while(True):
 	ret,frame = cam.read()
 
 	if ret:
-		# if video is still left continue creating images
-		name = './Pictures' + str(currentframe) + '.jpg'
-		print ('Creating...' + name)
-
-		# writing the extracted images
-		cv2.imwrite(name, frame)
-
-		# increasing counter so that it will
-		# show how many frames are created
-		currentframe += 1
+		if face_extractor(frame) is not None:
+			count+=1
+			face = cv2.resize(face_extractor(frame), (200,200))
+			face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)    
+			file_name_path = f'data/train/client001_{str(count)}.jpg'
+			cv2.imwrite(file_name_path, face)
+			cv2.putText(face, str(count), (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+			cv2.imshow('Face Cropper', face)
+    
+		else:
+			print("Face not found")
+			pass
 	else:
-		break
+		# Release all space and windows once done
+		cam.release()
+		cv2.destroyAllWindows()
 
-# Release all space and windows once done
-cam.release()
-cv2.destroyAllWindows()
+
+
 
