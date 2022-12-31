786c80551973afbced29c2a52181e37fa2fc85d2
AHazimy <aly.hazimy.95@gmail.com>
2022-12-31 22:37:45 +0200

    Model Finshed

--- FaceDetection/frames_extactor.py
+++ FaceDetection/frames_extactor.py
@@ -6,44 +6,43 @@ from api import face_extractor
 
 face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# Read the video from specified path
-cam = cv2.VideoCapture("./db_train_real/client001_session01_webcam_authenticate_adverse_1.mov")
-
 try:
-	
-	# creating a folder named data
 	if not os.path.exists('data/train'):
 		os.makedirs('data/train')
-
-# if not created then raise error
 except OSError:
 	print ('Error: Creating directory of data')
 
 
-count = 0
+data_path = './db_train_real/'
+videos = os.listdir(data_path)
 
-while(True):
+for video in videos:
+
+	cam = cv2.VideoCapture(os.path.join(data_path,video))
+
+
+	count = 0
+
+	while(True):
 	
-	# reading from frame
-	ret,frame = cam.read()
+		ret,frame = cam.read()
 
-	if ret:
-		if face_extractor(frame) is not None:
-			count+=1
-			face = cv2.resize(face_extractor(frame), (200,200))
-			face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)    
-			file_name_path = f'data/train/client001_{str(count)}.jpg'
-			cv2.imwrite(file_name_path, face)
-			cv2.putText(face, str(count), (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
-			cv2.imshow('Face Cropper', face)
+		if ret:
+			if face_extractor(frame) is not None:
+				count+=1
+				face = cv2.resize(face_extractor(frame), (200,200))
+				face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)    
+				file_name_path = f'data/train/client001_{str(count)}.jpg'
+				cv2.imwrite(file_name_path, face)
+				cv2.putText(face, str(count), (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+				cv2.imshow('Face Cropper', face)
     
+			else:
+				print("Face not found")
+				pass
 		else:
-			print("Face not found")
-			pass
-	else:
-		# Release all space and windows once done
-		cam.release()
-		cv2.destroyAllWindows()
+			cam.release()
+			cv2.destroyAllWindows()
 
 
 
