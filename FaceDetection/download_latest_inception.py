234f673ff06aeb3b2077c86ef48659a714edf11c
AHazimy <aly.hazimy.95@gmail.com>
2022-12-31 20:36:30 +0200

    Test

--- FaceDetection/download_latest_inception.py
+++ FaceDetection/download_latest_inception.py
@@ -1,3 +1,3 @@
-from keras.applications import InceptionV3
+from keras.applications.inception_v3 import InceptionV3
 
 inc = InceptionV3(input_shape=(224,224,3), weights='imagenet', include_top=False)
